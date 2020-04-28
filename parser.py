113934b53df67562645cb4a01f06ff9490c82370
Ілля <36272644+Eff1c@users.noreply.github.com>
2020-04-28 19:19:34 +0300

    add print url

--- parser.py
+++ parser.py
@@ -1,19 +1,24 @@
 import instaloader
+from os import getcwd
 
 L = instaloader.Instaloader()
 
-L.login("login", "password")
+L.load_session_from_file("cmvkdvldvclvxv@gmail.com", "/home/eff1c/project/first_parser/freelance/session")
 
-profile = instaloader.Profile.from_username(L.context, "nickname") # nickname of the person whose page you want to analyze
+profile = instaloader.Profile.from_username(L.context, "eff1c") # nickname of the person whose page you want to analyze
 
-following = []
-for followee in profile.get_followees():
-    following.append(followee.username)
 
-followers = []
-for follower in profile.get_followers():
-    followers.append(follower.username)
+following = [followee for followee in profile.get_followees()]
 
-output = list(set(following) - set(followers))
-for i in output:
-    print("https://www.instagram.com/" + i + "/")
+followers = [follower for follower in profile.get_followers()]
+
+unfollow = list(set(following) - set(followers))
+output = []
+for i in unfollow:
+	href = str(i).split()[1]
+	output.append("<p><a href='https://www.instagram.com/" + href + "/' target='_blank'>" + href + "</a></p>")
+
+with open("list_unfollow.html", "w") as f:
+	f.write("\n\n".join(output))
+
+print("file://" + getcwd() + "/list_unfollow.html")
